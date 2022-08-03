d3349c2c3567f261788ffac965339856f7583185
Benjamel <benjamin.meldal@gmail.com>
2022-08-03 23:19:36 +0200

    Question 1

--- js/script.js
+++ js/script.js
@@ -15,13 +15,15 @@ const cats = [
 
 //Question 1
 
-const cat = [
-    {
-        complain: function () {
-
-        }
+const cat = {
+    complain: function () {
+        console.log("Meow!");
     }
-];
+};
 
 
+cat.complain();
+
+
+//Question 2
 
