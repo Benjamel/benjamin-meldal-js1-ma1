7759804e84185c775b47c81efb4d6696f1e6609f
Benjamel <benjamin.meldal@gmail.com>
2022-08-03 20:55:53 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+
+//Question 1
+
+const cat = [
+    {
+        complain: function () {
+
+        }
+    }
+];
+
+
+
